66204688f5df83aa445ebad0ebc5b03e39652739
hassan kh salar <khsalarhassan@gmail.com>
2021-02-07 01:59:08 -0600

    lab task day 1

---
+++ Lab task 1-2/lab task 1-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab_task_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j, sum = 0;
+
+            Console.Write("\n\n");
+            Console.Write("Find the sum of first 100 odd even numbers:\n");
+            Console.Write("################################################");
+            Console.Write("\n\n");
+
+            Console.Write("The first 100 odd even number are :\n");
+            for (j = 1; j <= 100; j++)
+            {
+                sum = sum + j;
+                Console.Write("{0} ", j);
+            }
+            Console.Write("\nThe Sum is : {0}\n", sum);
+        }
+    }
+}
---
+++ lab task 1/lab task 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n =5 , sum = 0;
+
+            Console.Write("\n\n");
+            Console.Write("show even number:\n");
+            Console.Write("###########################################");
+            Console.Write("\n\n");
+
+            Console.Write("\nThe even numbers are :");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ", 2 * i);
+                sum += 2 * i;
+            }
+        }
+    }
+}
